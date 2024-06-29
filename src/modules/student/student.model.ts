1f73c08df1c4ef550ca068dac1afb8b766015de2
Ashiqur Rahman <imashiqe@gmail.com>
2024-06-29 15:57:03 +0600

    student  model  schema  created

--- src/modules/student/student.model.ts
+++ src/modules/student/student.model.ts
@@ -1,13 +1,25 @@
-import { profile } from 'console';
-import { Schema, model, connect } from 'mongoose';
+import { Schema } from 'mongoose';
+import { Guardian, Student, UserName } from './student.interface';
 
+// schema  start
+const userNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+//schema end
 const studentSchema = new Schema<Student>({
   id: { type: String, required: true },
-  name: {
-    firstName: { type: String, required: true },
-    middleName: { type: String },
-    lastName: { type: String, required: true },
-  },
+  name: userNameSchema,
   gender: ['male', 'female'],
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
@@ -16,14 +28,7 @@ const studentSchema = new Schema<Student>({
   bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: {
-    fatherName: { type: String, required: true },
-    fatherOccupation: { type: String, required: true },
-    fatherContactNo: { type: String, required: true },
-    motherName: { type: String, required: true },
-    motherOccupation: { type: String, required: true },
-    motherContactNo: { type: String, required: true },
-  },
+  guardian: guardianSchema,
   localGuardian: {
     name: { type: String, required: true },
     occupation: { type: String, required: true },
@@ -31,5 +36,5 @@ const studentSchema = new Schema<Student>({
     address: { type: String, required: true },
   },
   profileImg: { type: String },
-  isActive: ['active', 'block'],
+  isActive: ['active', 'blocked'],
 });
