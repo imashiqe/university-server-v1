ce99f615961d36a1421f4c97b5b6b140a937fd1b
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-01 14:13:44 +0600

    Validation  fix students model

--- src/modules/student/student.model.ts
+++ src/modules/student/student.model.ts
@@ -8,42 +8,95 @@ import {
 
 // schema  start
 const userNameSchema = new Schema<UserName>({
-  firstName: { type: String, required: true },
-  middleName: { type: String },
-  lastName: { type: String, required: true },
+  firstName: {
+    type: String,
+    required: [true, 'First Name is required'],
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last Name is required'],
+  },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  fatherName: { type: String, required: true },
-  fatherOccupation: { type: String, required: true },
-  fatherContactNo: { type: String, required: true },
-  motherName: { type: String, required: true },
-  motherOccupation: { type: String, required: true },
-  motherContactNo: { type: String, required: true },
+  fatherName: {
+    type: String,
+    required: [true, 'Father Name is required'],
+  },
+  fatherOccupation: {
+    type: String,
+    required: [true, 'Father Occupation is required'],
+  },
+  fatherContactNo: {
+    type: String,
+    required: [true, 'Father Contact Number is required'],
+  },
+  motherName: {
+    type: String,
+    required: [true, 'Mother Name is required'],
+  },
+  motherOccupation: {
+    type: String,
+    required: [true, 'Mother Occupation is required'],
+  },
+  motherContactNo: {
+    type: String,
+    required: [true, 'Mother Contact Number is required'],
+  },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String, required: true },
-  occupation: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  address: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, 'Local Guardian Name is required'],
+  },
+  occupation: {
+    type: String,
+    required: [true, 'Local Guardian Occupation is required'],
+  },
+  contactNumber: {
+    type: String,
+    required: [true, 'Local Guardian Contact Number is required'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Local Guardian Address is required'],
+  },
 });
 //schema end
 const studentSchema = new Schema<Student>({
-  id: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
   name: {
     type: userNameSchema,
-    required: true,
+    required: [true, 'Student Name is required'],
   },
   gender: {
     type: String,
-    enum: ['male', 'female', 'other'],
-    required: true,
+    enum: {
+      values: ['male', 'female', 'other'],
+      message: "The gender should be 'male', 'female', or 'other'",
+    },
+    required: [true, 'Gender is required'],
   },
-  dateOfBirth: { type: String, required: true },
-  email: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  emergencyContact: { type: String, required: true },
+  dateOfBirth: {
+    type: String,
+    required: [true, 'Date of Birth is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  contactNumber: {
+    type: String,
+    required: [true, 'Contact Number is required'],
+  },
+  emergencyContact: {
+    type: String,
+    required: [true, 'Emergency Contact is required'],
+  },
   bloodGroup: {
     type: String,
     enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
