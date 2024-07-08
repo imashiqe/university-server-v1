cfb0e7be869a5a50a7d1b3fe76ffb0c94c170544
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-08 14:36:55 +0600

    user interface updated

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type Tuser = {
+  id: string;
+  password: string;
+  needPasswordChange: boolean;
+  role: 'admin' | 'student' | 'faculty';
+  status: 'in-progress' | 'blocked';
+  isDeleted: boolean;
+};
