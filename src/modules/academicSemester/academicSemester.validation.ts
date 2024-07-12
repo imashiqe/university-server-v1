e9e0544708fa8a1efb31df4c673a00d87a43cf47
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-12 16:34:20 +0600

    Academic Semester Validation Added

--- src/modules/academicSemester/academicSemester.validation.ts
+++ src/modules/academicSemester/academicSemester.validation.ts
@@ -1,9 +1,18 @@
 import { z } from 'zod';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Month,
+} from './academicSemester.constant';
 
 const academicSemesterValidationSchema = z.object({
-    body: z.object({
-        name: z.enum(['Autumn',  'Summer', 'Fall'])
-    })
+  body: z.object({
+    name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
+    year: z.date(),
+    code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
+    startMonth: z.enum([...Month] as [string, ...string[]]),
+    endMonth: z.enum([...Month] as [string, ...string[]]),
+  }),
 });
 
 export const AcademicSemesterValidation = {
