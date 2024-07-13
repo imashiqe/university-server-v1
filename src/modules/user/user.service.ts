c46729369789c3078822a3e2eb95cf474f525db0
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-13 16:50:23 +0600

    student id pattern

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,7 @@
 import { AcademicSemester } from './../academicSemester/academicSemester.model';
 import config from '../../app/config';
-import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
-
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
-
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
@@ -26,7 +23,7 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester
   );
 
-  userData.id = generateStudentId(admissionSemester);
+  userData.id = await generateStudentId(admissionSemester);
   // create a user
   const newUser = await User.create(userData);
 
