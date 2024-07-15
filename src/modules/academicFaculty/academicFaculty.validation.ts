a654c3d15c674008bce1cb209fd3b537172917c9
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-15 15:53:50 +0600

    Academic Faculty Validation added

--- src/modules/academicFaculty/academicFaculty.validation.ts
+++ src/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,18 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty must be string',
+    }),
   }),
 });
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty must be string',
+    }),
   }),
 });
 
